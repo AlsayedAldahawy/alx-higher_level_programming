049a36f3fc8a21f32d1637a233c6dc9721419744
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-12-19 23:43:23 +0200

    4.

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,49 +1,42 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Area of a square
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+    """
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
-    def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            None
-        """
-        self.size = size
+    """Area of a square
 
-    def area(self):
-        """calculates the square's area
-        Returns:
-            The area of the square
-        """
-        return (self.__size) ** 2
+    Attributes:
+        __size (int): the size of square
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     @property
     def size(self):
-        """getter of __size
-        Returns:
-            The size of the square
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """setter of __size
-        Args:
-            value (int): the size of a size of the square
-        Returns:
-            None
-        """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
+    def area(self):
+        """Get current square area"""
+        return self.__size ** 2
