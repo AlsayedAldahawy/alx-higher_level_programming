e0d2e4ef63bec54dce97f869f86f254c5b201232
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-01-13 20:55:22 +0200

    prctice python 0

--- practice/0-notes.py
+++ practice/0-notes.py
@@ -11,6 +11,7 @@ experience = 5
 graduation_year = 2019
 siblings = 2
 
+print('''----the modulo operator % Method----''')
 
 'print("Hello, %s!, you are %d" %(name, age))'
 
@@ -18,3 +19,6 @@ print("""Hello, %(n)s!, you are %(a)d yo. \nyou are %(s)s working as %(p)s, with
 \nyou have graduated in %(g)d \nyou have %(sib)d siblings""" %{"n": name, 'a': age, 's': status, 'p':profession, 'x': experience,'sib': siblings, 'g': graduation_year})
 
 
+print('''----The str.format() Method----''')
+print("""Hello, {}!, you are {} yo. \nyou are {} working as {}, with {} years experience. \
+\nyou have graduated in {} \nyou have {} siblings""".format(name, age, status, profession, experience, graduation_year, siblings))
