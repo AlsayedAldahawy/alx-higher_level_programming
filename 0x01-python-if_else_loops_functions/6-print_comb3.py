b6397c0f85af93119a90ac476ed8ddbb82ded1fc
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-14 15:13:00 +0200

    rev-py0106

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -17,7 +17,7 @@ You are not allowed to import any module
 
 for i in range(9):
     for j in range(i + 1, 10):
-        print("{}{}".format(i,j), end=(""))
-        if not(i == 8 and j == 9):
+        print("{}{}".format(i, j), end=(""))
+        if not (i == 8 and j == 9):
             print(', ', end=(""))
 print("")
