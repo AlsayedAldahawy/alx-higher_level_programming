a31cde2e581018f66dcadd9ade993e9a686aa5c5
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-14 16:06:48 +0200

    rev-py0108

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for ch in str:
-        if ord(ch) >= 97 and ord(ch) <= 122:
-            ch = chr(ord(ch) - 32)
-        print("{:s}".format(ch), end='')
 
-    print('\n', end="")
+'''
+Write a function that prints a string in uppercase followed by a new line.
+
+Prototype: def uppercase(str):
+You can only use no more than 2 print functions with string format
+You can only use one loop in your code
+You are not allowed to import any module
+You are not allowed to use str.upper() and str.isupper()
+
+'''
+
+
+def uppercase(str):
+
+    c = 0
+
+    for i in str:
+        if i >= 'a' and i <= 'z':
+            c = ord(i) - 32
+        else:
+            c = ord(i)
+        print("{}".format(chr(c)), end=(""))
+    print("")
