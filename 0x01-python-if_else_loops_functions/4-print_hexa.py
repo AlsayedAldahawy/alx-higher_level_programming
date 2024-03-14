ee0dfa6a364a1cbcb81220fbe03a2f22f93d4a4d
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-14 06:36:34 +0200

    rev-py0104

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print('{0} = {1}'.format(num, hex(num)))
+
+'''
+Write a program that prints all numbers from 0 to 98
+in decimal and in hexadecimal
+'''
+for i in range(0, 99):
+    print("{:d} = {}".format(i, hex(i)))
