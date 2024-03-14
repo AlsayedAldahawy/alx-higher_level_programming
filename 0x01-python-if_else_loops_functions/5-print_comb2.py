d8a0682829528fb81140dbbf2b1d97f3e6823b35
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-14 14:58:33 +0200

    rev-py0105

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i + 1))
+
+'''
+Write a program that prints numbers from 0 to 99.
+'''
+
+for i in range(99):
+    print("{:02d}, ".format(i), end=(""))
+print("{}".format(i + 1))
