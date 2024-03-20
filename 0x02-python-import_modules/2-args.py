d8c726e10e2e58e1af514a08d3e8931c869f2cc2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-20 15:58:50 +0200

    rev-p0202

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
+    elif len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
     i = 1
