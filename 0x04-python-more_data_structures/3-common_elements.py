b510accf20399cff83431a966cb0fad5a36170d2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-24 17:30:33 +0200

    rev-py0403

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,9 +2,11 @@
 def common_elements(set_1, set_2):
     # 1- using loops
 
-    newList = []
+    # newList = []
+    # for i in set_1:
+    #     if i in set_2:
+    #         newList.append(i)
+    # return newList
 
-    for i in set_1:
-        if i in set_2:
-            newList.append(i)
-    return newList
+    # 2- using set operations (&, |, -, +, ^)
+    return set_1 & set_2
