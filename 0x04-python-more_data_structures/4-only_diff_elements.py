b9af1a2d1b15702858eda6dadf96abb26ab93de2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-24 17:40:44 +0200

    rev-py0404

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     # 1- using loops
-
     # newList = []
     # for i in set_1:
-    #     if i in set_2:
+    #     newList.append(i)
+    # for i in set_2:
+    #     if i not in set_1:
     #         newList.append(i)
     # return newList
 
     # 2- using set operations (&, |, -, ^)
-    return set_1 & set_2
+    return set_1 | set_2
