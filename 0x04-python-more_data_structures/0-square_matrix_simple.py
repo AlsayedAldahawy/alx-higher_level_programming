c67ef188859f91ff3c3bd314dee7035f9db4cf28
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-24 16:12:55 +0200

    rev-py0400

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # 1- using Nested List Comprehensions
-    # new_matrix = [[(x ** 2) for x in row] for row in matrix]
+    new_matrix = [[(x ** 2) for x in row] for row in matrix]
 
     # 2- using loops
     # new_matrix = []
@@ -11,10 +11,10 @@ def square_matrix_simple(matrix=[]):
     #         sub.append(i ** 2)
     #     new_matrix.append(sub)
 
-    # 3- using lambda
+    # 3- using lambda & map
+    # new_matrix = []
+    # for row in matrix:
+    #     new_row = list(map(lambda x: x ** 2, row))
+    #     new_matrix.append(new_row)
 
-    new_matrix = []
-    for row in matrix:
-        new_row = list(map(lambda x: x ** 2, row))
-        new_matrix.append(new_row)
     return new_matrix
