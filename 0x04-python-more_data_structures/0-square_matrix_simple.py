a80ae5358525ee15f94538c0181a71ed25da00f7
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-24 15:54:07 +0200

    rev-py0400

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[(x ** 2) for x in row] for row in matrix]
+    # new_matrix = [[(x ** 2) for x in row] for row in matrix]
+
+    new_matrix = []
+
+    for row in matrix:
+        sub = []
+        for i in row:
+            sub.append(i ** 2)
+        new_matrix.append(sub)
     return new_matrix
