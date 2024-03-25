cab9b20796673c30e3d8021473fd6ebc7b84b5f7
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-03-25 14:27:35 +0200

    rev-py0415

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
 
-    keys = []
-    for key, val in a_dictionary.items():
-        if val == value:
-            keys.append(key)
+    # for key, val in a_dictionary.items():
+    #     if val == value:
+    #         keys.append(key)
+
+    # using list comprehension
+    keys = [key for key, val in a_dictionary.items() if val == value]
+
     for key in keys:
         del a_dictionary[key]
     return a_dictionary
