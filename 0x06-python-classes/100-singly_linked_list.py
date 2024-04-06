a891486ecc05c0997c7e31f8fed9e65de0e20efb
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-06 16:59:18 +0200

    rev-py0607_sorted_insert_method

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,6 +2,7 @@
 
 '''square class'''
 
+
 class Node:
     def __init__(self, data, next_node=None):
 
@@ -18,7 +19,7 @@ class Node:
 
     @next_node.setter
     def next_node(self, value=None):
-        if value != None and not isinstance(value, Node):
+        if value is not None and not isinstance(value, Node):
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
@@ -27,21 +28,30 @@ class Node:
         if not isinstance(value, int):
             raise TypeError('data must be an integer')
         self.__data = value
-        
+
+
 class SinglyLinkedList:
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
         new_node = Node(value)
-        if self.__head == None:
-            new_node.data = value
+        if self.__head is None:
+            # If the list is empty, insert the new node as the head
             new_node.next_node = None
             self.__head = new_node
+        elif value <= self.__head.data:
+            # If the value is less than or equal to the head,
+            # insert at the beginning
+            new_node.next_node, self.__head = self.__head, new_node
         else:
-            new_node.data = value
-            new_node.next_node = self.__head
-            self.__head = new_node
+            # Traverse the list to find the correct position
+            curr = self.__head
+            while curr.next_node and value > curr.next_node.data:
+                curr = curr.next_node
+            # Insert the new node after the current node
+            new_node.next_node = curr.next_node
+            curr.next_node = new_node
 
     def __str__(self):
         """Define the print() representation of a SinglyLinkedList."""
@@ -59,11 +69,3 @@ class SinglyLinkedList:
             string += (str(current.data)) + '\n'
             current = current.next_node
         return string[:-1]
-
-    
-
-
-    
-        
-        
-
