bc5fb198a281afb9b5e86c3843dee2221afc4206
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-14 20:05:31 +0200

    rev-py0704

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -26,6 +26,11 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     i = 0
+
+    # removing the indentation in the begining
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
     while i < len(text):
         print(text[i], end="")
         if text[i] in ['.', '?', ':']:
