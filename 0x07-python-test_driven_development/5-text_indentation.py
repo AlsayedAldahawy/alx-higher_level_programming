4f1bd4bca3fc8e73e1b78d152bfafb1f29708d8a
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-14 19:58:36 +0200

    rev-py0704

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    """
+    Prints the provided text with 2 new lines after each occurrence
+        of '.', '?', and ':'.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If `text` is not a string.
+
+    Example:
+        >>> text_indentation("Hello. How are you? I'm fine.")
+        Hello
+        How are you
+        I'm fine.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ['.', '?', ':']:
+            print("\n")
+            while i + 1 < len(text) and text[i + 1] in [' ', '\n']:
+                i += 1
+        i += 1
