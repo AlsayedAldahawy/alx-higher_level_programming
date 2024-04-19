52928c877442003e36399548d7fe47ebf786b01f
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-20 01:24:20 +0200

    rev-py0804

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -82,13 +82,13 @@ class Rectangle:
         Returns:
             str: String containing '#' symbols forming the rectangle.
         """
-    
+
         if self.height == 0 or self.width == 0:
             return ""
 
         # Create a string with '#' symbols for each row
         rect_str = "\n".join(["#" * self.width for _ in range(self.height)])
         return rect_str
-    
+
     def __repr__(self):
         return "Rectangle(" + str(self.width) + "," + str(self.height) + ")"
