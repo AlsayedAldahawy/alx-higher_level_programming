ece84f1929a20d0dafcd22656b591dd832180b5a
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-21 01:57:05 +0200

    rev-py0930

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python3
 
 """
-Module contains LockedClass class.
+    Module contains LockedClass class.
 """
 
 
+class LockedClass:
+    """
+    __slots__ is a special attribute that takes a certain pool of attribute
+        names that can be used.
+
+        Any attributes will be dynamically created by the user needs to have
+        a name of the specified ones in __slot__ list,
+        otherwise it will raise an AttributeError.
+
+        even built-in attributes such as "__dict__" will not be avilable unless
+        it was added to the __slots__ list.
+    """
+
+    __slots__ = ["first_name"]
+
+
+'''
+    in case we only wanted to prevent the user from creating attributes with
+    keeping the built-in attributes "__dict__", we can use __settatr__ method.
+'''
+
+# the next example passes all the checker testcases except no.7,
+# which uses __dict__ attribute in the testcase
+
+'''
 class LockedClass:
     """
     A class that allows only the 'first_name' attribute to be set.
@@ -29,3 +54,4 @@ class LockedClass:
                 f"'LockedClass' object has no attribute '{name}'")
         else:
             super().__setattr__(name, value)
+'''
