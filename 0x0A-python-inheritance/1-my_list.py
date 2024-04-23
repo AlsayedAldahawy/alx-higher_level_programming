04cc1b75e1c2fc040f279c0aa8c3afc2cb8eab8b
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-23 02:33:03 +0200

    rev-py0A01

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+class MyList(list):
+    '''
+        class inherit from list object
+        it has method: print_sorted that prints the list, but sorted.
+    '''
+
+    def print_sorted(self):
+        print(sorted(self))
