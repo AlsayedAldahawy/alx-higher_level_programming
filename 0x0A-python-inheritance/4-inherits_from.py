d2cbcae339e26fe211db40d175ca070897f31f46
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-23 18:36:44 +0200

    rev-py0A04

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
     Returns:
         _type_: True of the same, otherwise False.
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
