161df5fdb55f3bfcac6161c8198fd67a259315b6
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-04-24 01:24:44 +0200

    rev-py0A11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+"""
+Module: 11-square.py
+
+This module defines the Square class, which inherits from the Rectangle class.
+
+Classes:
+    - Square: Represents a square with equal sides.
+
+Example usage:
+    >>> square = Square(5)
+    >>> square.area()
+    25
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class represents a square with equal sides.
+
+    Attributes:
+        - __size (int): The size of the square's sides.
+
+    Methods:
+        - __init__(self, size): Initializes a Square instance with the\
+            given size.
+        - area(self): Calculates and returns the area of the square.
+
+    Example:
+        >>> square = Square(5)
+        >>> square.area()
+        25
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with the given size.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        Example:
+            >>> square = Square(5)
+            >>> square.area()
+            25
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square.
+
+        Returns:
+            str: A formatted string representing the Square.
+
+        Example:
+            >>> square = Square(5, 3)
+            >>> print(square)
+            [Square] 5/3
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
