09ff5d61f04ad56c370746feaa8434d8c70924b9
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-06 03:57:31 +0300

    rev-c0A03

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import json
+
+"""
+    function that returns the JSON representation of an object (string):
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Converts a Python object (my_obj) to a JSON-formatted string.
+
+    Args:
+        my_obj: The Python object to be converted.
+
+    Returns:
+        str: A JSON-formatted string representing the input object.
+
+    Example:
+        >>> data = {"name": "Alice", "age": 30}
+        >>> json_string = to_json_string(data)
+        >>> print(json_string)
+        '{"name": "Alice", "age": 30}'
+    """
+    return json.dumps(my_obj)
