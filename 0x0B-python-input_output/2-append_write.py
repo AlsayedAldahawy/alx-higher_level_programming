20c7a436869e4fae51d4ee06bf04ee34a8f20977
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-06 03:49:34 +0300

    rev-c0A02

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Function that appends a string at the end of a text file (UTF-8) and returns
-# the number of characters added:
+
+""" Function that appends a string at the end of a text file (UTF-8) and returns\
+        the number of characters added:"""
 
 def append_write(filename="", text=""):
     """
