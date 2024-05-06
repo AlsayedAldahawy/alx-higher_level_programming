9aba6f44b044cbdad9f7540b0db05bcef4dd35cf
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-06 03:31:02 +0300

    rev-c0A00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python3
-"""
-Contains the read_file function
-"""
-
+#!/usr/bin/env python3
+# a function that reads a text file (UTF8) and prints it to stdout:
 
 def read_file(filename=""):
-    """""reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
+
