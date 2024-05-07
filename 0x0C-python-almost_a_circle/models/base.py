9290cf2d1c1e9fe0b3166327d701b8ff07142b8e
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-08 01:08:48 +0300

    rev-py0C15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,6 +57,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ return json string"""
         if not list_dictionaries:
