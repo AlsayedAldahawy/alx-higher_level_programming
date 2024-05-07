5bcfeee90c2fa3fa0a34caf32c8768712f74e8e4
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-07 23:00:39 +0300

    rev-py0C10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+    square module
+'''
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns the print() and str() representation of the Square."""
+        return "[Square] (%d) %d/%d - %d" % (self.id, self.x,
+                                             self.y, self.width)
