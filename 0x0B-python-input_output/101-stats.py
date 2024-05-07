2a17bf6038585378dbb89f1d937c10f5d7266088
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-07 04:51:15 +0300

    rev-py0B14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,44 +1,66 @@
 #!/usr/bin/python3
-# a script that reads stdin line by line and computes metrics:
-# Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-# <status code> <file size>
+
 
 '''
-module doc
+Module: 101-stats.py module
+This module provides functions for processing log data and printing statistics.
 '''
+
 import sys
 
 
 def print_stats(file_size, status_dict):
-    print("File size: %d" % (file_size))
+    '''
+    Prints the total file size and a sorted list of status codes along with\
+        their counts.
+
+    Args:
+        file_size (int): Total file size.
+        status_dict (dict): Dictionary of status codes and their counts.
+
+    Returns:
+        None
+    '''
+    print("File size:", file_size)
+
+    # Sorting the dictionary
     status_dict = {key: status_dict[key] for key in sorted(status_dict)}
+
+    # print the dictionary as pairs of <key>: <value>
     for key, value in zip(status_dict.keys(), status_dict.values()):
         print(f"{key}: {value}")
 
 
-count = 0
-file_size = 0
-status_dict = {}
+count = 0           # Initialization of lines counter
+file_size = 0       # Initialization of file_size to handle the file size
+status_dict = {}    # Initialization of empty dict to handle the status data
 
 try:
+    # To handle the KeyboardInterrupt, we need to use exceptions
+
+    # Taking lines from the stdin
     for line in sys.stdin:
 
         if count == 10:
             print_stats(file_size, status_dict)
             count = 0
+        count += 1
 
         try:
+            # If file size is not a valid value or not exist, pass
             file_size += int(line.split()[-1])
-            status_code = int(line.split()[-2])
         except (IndexError, ValueError):
             pass
 
-        count += 1
-
-        if status_code not in status_dict:
-            status_dict[status_code] = 1
-        else:
-            status_dict[status_code] += 1
+        try:
+            # If status_code is not a valid value or not exist, pass
+            status_code = int(line.split()[-2])
+            if status_code not in status_dict:
+                status_dict[status_code] = 1
+            else:
+                status_dict[status_code] += 1
+        except (ValueError, IndexError):
+            pass
 
     print_stats(file_size, status_dict)
 
