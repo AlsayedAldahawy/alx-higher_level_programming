122850b8343fa8b18ed07fbc8072f6e26b1ae1fb
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-08 00:44:40 +0300

    rev-py0C12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,3 +28,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        ''' update'''
+
+        try:
+            for key, value in kwargs.items():
+                if key in ["size", "x", "y", "id"]:
+                    setattr(self, key, value)
+        except Exception:
+            raise
+
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            pass
