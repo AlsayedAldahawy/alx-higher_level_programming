4b2758a5edda87473f72592d8adee0f9ca8465d6
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-23 23:26:09 +0300

    rev-MySQL-00

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++
@@ -1,8 +0,0 @@
--- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SELECT
-    `city`, avg(`value`) AS avg_temp
-FROM temperatures
-GROUP by
-    `city`
-ORDER BY
-    `avg_temp` DESC;
