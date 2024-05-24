4d81670dab348d1d11d6c6ed050b74a90efd1959
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-24 05:05:57 +0300

    rev-MySqlMore-07

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    FOREIGN KEY (state_id) REFERENCES states (state_id) INT NOT NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states (state_id)
 );
