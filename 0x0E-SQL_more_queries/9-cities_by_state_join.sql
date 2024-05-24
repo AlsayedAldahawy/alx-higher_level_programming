8cdfb2f805fef482d80000916d54e9f798e35314
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-25 02:11:52 +0300

    rev-MySQLMore-10

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,6 @@
 -- Lists all cities in the database hbtn_0d_usa.
-SELECT
-    tv_shows.title,
-    tv_show_genres.genre_id
-FROM
-    tv_shows
-    inner join tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY
-    tv_shows.title,
-    tv_show_genres.genre_id;
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
