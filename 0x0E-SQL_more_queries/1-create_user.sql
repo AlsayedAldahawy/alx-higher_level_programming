57cd85be0e62c4236119377c33e5b4a19adf338c
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-24 04:30:35 +0300

    rev-MySqlMore-01

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script that creates the MySQL server user user_0d_1.
 -- And give him all privileges.
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES OF *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost';
