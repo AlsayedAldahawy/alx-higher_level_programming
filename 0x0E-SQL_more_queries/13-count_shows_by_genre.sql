3face4a7ed5ee8b87f0f1d1f6f482067a59bbc40
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-25 03:00:19 +0300

    rev-MySQLMore-13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@ SELECT
     g.NAME AS genre,
     count(*) AS number_of_shows
 FROM
-    tv_genres as g
+    tv_genres AS g
     INNER JOIN tv_show_genres AS g2 ON g.id = g2.genre_id
 GROUP BY
     genre
