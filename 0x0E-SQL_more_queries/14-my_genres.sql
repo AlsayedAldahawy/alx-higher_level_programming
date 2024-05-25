c17eaed22c67acef5ab177a8404cdf3708639d6d
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-25 03:14:01 +0300

    rev-MySQLMore-14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
+SELECT
+    tv_genres.name
+FROM
+    tv_genres
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE
+    tv_show_genres.show_id = 8;
