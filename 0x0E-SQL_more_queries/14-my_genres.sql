efef779671ea17b7d5322709881864af7059f89d
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-25 03:18:07 +0300

    rev-MySQLMore-14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,6 +4,6 @@ SELECT
 FROM
     tv_genres
     INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    inner join tv_shows on tv_shows.id = tv_show_genres.show_id
+    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE
     tv_shows.title = "Dexter";
