ed28632f689b192038d1ffb134e19d3793d5b6f1
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-25 03:31:45 +0300

    rev-MySQLMore-15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,9 +3,9 @@ SELECT
     s.title
 FROM
     tv_shows AS s
-    INNER JOIN tv_show_genres as sg ON s.id = sg.show_id
+    INNER JOIN tv_show_genres AS sg ON s.id = sg.show_id
     INNER JOIN tv_genres AS g ON sg.genre_id = g.id
-where
+WHERE
     g.name = "Comedy"
 ORDER BY
     s.title;
