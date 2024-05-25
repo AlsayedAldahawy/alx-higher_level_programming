5d9b3908ba14511735bce7eb5bb1c70c27edfffe
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-26 00:31:50 +0300

    rev-MySQLMore-18

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,16 @@
+-- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
+SELECT DISTINCT
+    s.title
+FROM
+    tv_shows AS s
+    LEFT JOIN tv_show_genres AS sg ON s.id = sg.show_id
+where
+    sg.genre_id NOT IN(
+        SELECT DISTINCT
+            sg.genre_id
+        FROM
+            tv_show_genres AS sg
+            INNER JOIN tv_genres AS g ON g.id = sg.genre_id
+        where
+            g.name = "Comedy"
+    );
