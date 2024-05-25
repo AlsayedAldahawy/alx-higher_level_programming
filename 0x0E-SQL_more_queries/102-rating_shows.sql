84d20cf228ec089206bdea22f5cb0493f1f32a5e
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-26 01:46:26 +0300

    rev-MySQLMore-19

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT s.title, sum(r.rate)
+FROM
+    tv_shows AS s
+    LEFT JOIN tv_show_ratings AS r ON s.id = r.show_id
+GROUP BY
+    s.title
+ORDER BY sum(r.rate) DESC;
