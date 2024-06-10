e60a6871cc2cfbe72428f751ad037faa41d39197
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-11 01:21:15 +0300

    rev-python-SQL-0x0F-01

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-This script connects to a MySQL database and retrieves all rows from the 'states' table
+This script connects to a MySQL database and retrieves all rows from the
+    'states' table
 where the state name starts with the letter 'N'.
-It requires command-line arguments for the MySQL user, password, and database name.
+It requires command-line arguments for the MySQL user, password, and
+    database name.
 """
 
 import MySQLdb
@@ -10,13 +12,15 @@ from sys import argv
 
 if __name__ == "__main__":
     # Establish a connection to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
 
     # Create a cursor to execute SQL queries
     cur = db.cursor()
 
     # Execute a query to retrieve states whose names start with 'N'
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
 
     # Fetch all rows returned by the query
     rows = cur.fetchall()
