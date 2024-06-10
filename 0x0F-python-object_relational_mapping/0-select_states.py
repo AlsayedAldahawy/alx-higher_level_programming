b4497ede37adccb27716a8824e78f9a99e9382bc
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-11 01:05:26 +0300

    rev-python-SQL-0x0F-00

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database and retrieves all rows from the
+    'states' table.
+It requires command-line arguments for the MySQL user, password, and
+    database name.
+"""
+
 import MySQLdb
 from sys import argv
 
-if len(argv) != 3 and __name__ == "__main__":
-    
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+if __name__ == "__main__":
+    # Establish a connection to the MySQL database
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2],
+                           db=argv[3], charset="utf8")
+
+    # Create a cursor to execute SQL queries
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+
+    # Execute a query to retrieve all states from the 'states' table
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
+
+    # Print each row retrieved from the database
     for row in query_rows:
         print(row)
+
+    # Close the cursor and database connection
     cur.close()
     conn.close()
