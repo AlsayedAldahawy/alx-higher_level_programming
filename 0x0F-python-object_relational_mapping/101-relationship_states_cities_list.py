4338b8070f91272494242fdff65573ba7638423c
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-13 02:40:51 +0300

    rev-python-SQL-0x0F-15

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+This script establishes a one-to-many relationship between states and cities
+in a MySQL database.
+
+Usage:
+    - Ensure you have the necessary dependencies installed
+        (SQLAlchemy, MySQLdb).
+    - Run the script with the following command:
+        python3 script_name.py <username> <password> <database_name>
+
+Arguments:
+    <username> (str): MySQL username.
+    <password> (str): MySQL password.
+    <database_name> (str): Name of the database containing the "cities" and
+        "states" tables.
+
+Example:
+    python3 script_name.py myuser mypassword mydatabase
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from relationship_state import Base, State
+
+if __name__ == "__main__":
+    # Create a database connection
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query states and associated cities
+    states = session.query(State).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+
+    session.commit()
