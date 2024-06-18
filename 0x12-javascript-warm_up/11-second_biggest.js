4a078878625d6047583a63670e961a4add0aac7e
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-18 17:38:46 +0300

    rev-JS-0x12-00

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const args = process.argv.slice(2).map(Number);
-const args2 = args.sort(function (a, b) {
-  return b - a;
-});
-console.log(args2[1] ?? 0);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-const strk = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-strk.forEach(value => {
-  console.log(value);
-});
