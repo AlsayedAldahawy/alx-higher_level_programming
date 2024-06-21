25f0ba35a8994c4abad802ecbe72aaa646a47d8c
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-21 19:58:37 +0300

    rev-JS-0x13-12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const dict2 = {};
+for (const key in dict) {
+  if (dict[key] in dict2) {
+    dict2[dict[key]].push(key);
+  } else {
+    dict2[dict[key]] = [key];
+  }
+}
+console.log(dict2);
