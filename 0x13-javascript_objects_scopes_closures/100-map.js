6681fbc5d5eb5f32ebee445ed6c83519d7849e5a
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-21 13:06:12 +0300

    rev-JS-0x13-11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const list2 = list.map((x, i) => x * i);
+console.log(list);
+console.log(list.map((item, index) => item * index));
 // the index i is automatically incremented by the map itself for each iteration over the array.
-console.log(`${list}\n${list2}`);
